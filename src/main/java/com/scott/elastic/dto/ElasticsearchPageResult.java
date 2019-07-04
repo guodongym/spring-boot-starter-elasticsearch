82c26d498e14e1e7a76884c2eab923152f3dbcb8
zhaoguodong <zhaoguodong@bitnei.cn>
2019-07-04 18:39:39 +0800

    - 拓展ElasticsearchPageResult支持返回scrollId

--- src/main/java/com/scott/elastic/dto/ElasticsearchPageResult.java
+++ src/main/java/com/scott/elastic/dto/ElasticsearchPageResult.java
@@ -14,10 +14,24 @@ import java.util.List;
 @Data
 public class ElasticsearchPageResult<T> {
 
+    /**
+     * 总条数
+     */
     private Long totalCount;
 
+    /**
+     * 总条数的模式，等于或者大于
+     */
     private TotalHits.Relation relation;
 
+    /**
+     * 滚动id，导出时使用
+     */
+    private String scrollId;
+
+    /**
+     * 结果数据
+     */
     private List<T> data;
 
 }
